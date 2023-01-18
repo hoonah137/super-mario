2c0a4211988cff94d382081b0dd0954d7c958ad7
hoonah137 <121156401+hoonah137@users.noreply.github.com>
2023-01-18 10:05:36 +0100

    11

--- Assets/scripts/PlayerControler.cs
+++ Assets/scripts/PlayerControler.cs
@@ -8,16 +8,23 @@ public class PlayerControler : MonoBehaviour
    public float playerSpeed = 5.5f;
    string text = "Hello world";
    bool isGrounded = false;
+   public SpriteRenderer sRender ;
+   float horizontal;
+   Rigidbody2D rBody; 
+   public float jumpForce = 5.7f;
+
 
 //int = numero entero
 //float = nmero decimal
-// String = texto
+//String = texto
 //bool = cierto o falso
-// public + variable = te deja ver la variale en el inspector sin necesidad de modificar el script
+//public + variable = te deja ver la variale en el inspector sin necesidad de modificar el script
 
     // Start is called before the first frame update
     void Start()
     {
+        sRender = GetComponent<SpriteRenderer> ();
+        rBody = GetComponent<Rigidbody2D>();
         playerHealth = 10;
         Debug. Log(text);
         
@@ -26,6 +33,23 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      transform. position += new Vector3 (Input . GetAxis("Horizontal"), 0, 0) * playerSpeed * Time . deltaTime ;  
+      horizontal = Input . GetAxis("Horizontal");
+
+      transform. position += new Vector3 (horizontal, 0, 0) * playerSpeed * Time . deltaTime ;  
+
+      if (horizontal < 0 )
+      {
+        sRender.flipX = true;
+      }
+
+      else if (horizontal > 0 )
+      {
+        sRender.flipX = false;
+      }
+
+      if(Input.GetButtonDown("Jump"))
+      {
+        rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+      }
     }
 }
